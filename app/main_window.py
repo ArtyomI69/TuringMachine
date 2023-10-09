be933d16e62193fef9b79ad81d1ea13e7438adc7
nydus <nydusborn@outlook.com>
2023-10-09 22:54:17 +0300

    более хороший стиль

--- app/main_window.py
+++ app/main_window.py
@@ -7,17 +7,23 @@ class MyWidget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
+        self.setWindowTitle("Turing Machine")
+        self.setWindowIcon(QtGui.QIcon("Assets/icon.png"))
+
         self.program_page = QtWidgets.QWidget(self)
         self.program_page_ui()
-        self.memory_page = QtWidgets.QWidget(self)        
+        self.memory_page = QtWidgets.QWidget(self)
         self.memory_page_ui()
 
         self.layout = QtWidgets.QTabWidget(self)
         self.layout.addTab(self.program_page, "Program")
         self.layout.addTab(self.memory_page, "Memory")
-    
+
+        main_layout = QtWidgets.QVBoxLayout(self)
+        main_layout.addWidget(self.layout)
+
     def program_page_ui(self):
-        layout = QtWidgets.QVBoxLayout()
+        layout = QtWidgets.QVBoxLayout(self.program_page)
         fields = QtWidgets.QHBoxLayout()
         layout.addLayout(fields)
         text_program = QtWidgets.QTextEdit()
@@ -38,17 +44,22 @@ class MyWidget(QtWidgets.QWidget):
         layout.addLayout(timeline_overall)
         timeline_overall.addLayout(timeline_buttons)
         timeline_cells = QtWidgets.QHBoxLayout()
-        self.program_page.setLayout(layout)
+        timeline_overall.addLayout(timeline_cells)
+
     def memory_page_ui(self):
-        layout = QtWidgets.QVBoxLayout()
+        layout = QtWidgets.QVBoxLayout(self.memory_page)
         fields = QtWidgets.QHBoxLayout()
         layout.addLayout(fields)
         text_program = QtWidgets.QTextEdit()
+        io_vbox = QtWidgets.QVBoxLayout()
         text_input = QtWidgets.QTextEdit()
         text_output = QtWidgets.QTextEdit()
+        io_vbox.addWidget(text_input)
+        io_vbox.addWidget(text_output)
+        text_memory = QtWidgets.QTextEdit()
         fields.addWidget(text_program)
-        fields.addWidget(text_input)
-        fields.addWidget(text_output)
+        fields.addLayout(io_vbox)
+        fields.addWidget(text_memory)
         timeline_overall = QtWidgets.QVBoxLayout()
         timeline_buttons = QtWidgets.QHBoxLayout()
         timeline_button_back_to_start = QtWidgets.QPushButton("Back to start")
@@ -61,12 +72,13 @@ class MyWidget(QtWidgets.QWidget):
         layout.addLayout(timeline_overall)
         timeline_overall.addLayout(timeline_buttons)
         timeline_cells = QtWidgets.QHBoxLayout()
-        self.memory_page.setLayout(layout)
+        timeline_overall.addLayout(timeline_cells)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
+    app.setStyle('Fusion')
     widget = MyWidget()
     widget.resize(1600, 800)
     widget.show()

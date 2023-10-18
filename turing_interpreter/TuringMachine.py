53dec4fe5978383d1b9a5e456e5a340edf706b0b
UsikovKonstantin <usikov-kostya@mail.ru>
2023-10-18 18:28:58 +0300

    Рабочая версия машины

--- turing_interpreter/TuringMachine.py
+++ turing_interpreter/TuringMachine.py
@@ -13,6 +13,7 @@ class TuringMachine:
         prog = parse_program_file(program_file_name, self.__alphabet)
         self.__max_transitions = prog[0]
         self.__program = prog[1]
+        self.__step_count = 0
 
     @property
     def current_state(self):
@@ -45,3 +46,54 @@ class TuringMachine:
     @property
     def program(self):
         return self.__program
+
+    @property
+    def step_count(self):
+        return self.__step_count
+
+    def step(self):
+        current_letter = self.__tape_read_at(self.__current_index)
+        key = self.__current_state + " " + current_letter
+        if (key in self.__program) and (self.__step_count < self.__max_transitions):
+            next_state = self.__program[key][0]
+            letter = self.__program[key][1]
+            shift = self.__program[key][2]
+            self.__tape_write_at(self.__current_index, letter)
+            self.__shift(shift)
+            self.__current_state = next_state
+            self.__step_count += 1
+            return True
+        else:
+            return False
+
+    def run(self):
+        while self.step():
+            pass
+
+    def __shift(self, shift):
+        self.__current_index += shift
+        if self.__current_index >= 0 and (len(self.__tape_positive) == self.__current_index):
+            self.__tape_positive.append(self.__default_cell_state)
+        elif self.__current_index < 0 and (len(self.__tape_negative) == -self.__current_index - 1):
+            self.__tape_negative.append(self.__default_cell_state)
+
+    def __tape_write_at(self, index, letter):
+        if index >= 0:
+            if len(self.__tape_positive) > index:
+                self.__tape_positive[index] = letter
+            else:
+                self.__tape_positive.append(letter)
+        else:
+            if len(self.__tape_negative) > -index - 1:
+                self.__tape_negative[-index - 1] = letter
+            else:
+                self.__tape_negative.append(letter)
+
+    def __tape_read_at(self, index):
+        if index >= 0:
+            return self.__tape_positive[index]
+        else:
+            return self.__tape_negative[-index - 1]
+
+    def __str__(self):
+        return " ".join(self.__tape_negative[::-1] + self.tape_positive)

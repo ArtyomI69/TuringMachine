6d2b3277637c387472d03f2238bf23709af3efca
UsikovKonstantin <usikov-kostya@mail.ru>
2023-10-23 18:40:32 +0300

    Убрал ненужные методы

--- ConsoleTest/main.py
+++ ConsoleTest/main.py
@@ -1,7 +1,8 @@
 from turing_interpreter.TuringMachine import TuringMachine
 
 if __name__ == '__main__':
-    tm = TuringMachine("state.tms", "program.tmt")
+    tm = TuringMachine("@current_state: state_seek\n""@current_index: 0\n""@alphabet: _ 0 1\n""@default_cell_state: _\n""#------------------------\n""0: _ 1 0 1 0 0 1 1 0 _ 1",
+                       "@max_transitions: 100\n""state_seek _ -> state_seek _ R\n""state_seek 0 -> state_invert 0 N\n""state_seek 1 -> state_invert 1 N\n""state_invert 0 -> state_invert 1 R\n""state_invert 1 -> state_invert 0 R\n""state_invert _ -> state_stop _ N")
     # print(tm.current_state)
     # print(tm.current_index)
     # print(tm.alphabet)
